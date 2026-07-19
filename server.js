9ea897a762c1129869dc54d118afc0930bfafe63
Lisandro <lisandrosamuelpenalugo@gmail.com>
2026-07-19 09:55:37 -0400

    .

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ let express = require("express");
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-    res.status(200).sendfile("public/index.html");
+    res.status(200).send("Hello World!");
 });
+
 module.exports = app;
