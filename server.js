edcdda5e6c09791bc2c318863426f39cb7d768a1
Lisandro <lisandrosamuelpenalugo@gmail.com>
2026-07-18 20:08:59 -0400

    attempt 1

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
 app.use(express.static("public"));
 
 app.listen(3000, () => {
