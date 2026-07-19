549e214a59a35ee0ea0e6e422eb4ba6ae8fbea53
Lisandro <lisandrosamuelpenalugo@gmail.com>
2026-07-19 09:13:26 -0400

    attempt 5

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 const express = require("express");
-
-const desiredport = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 })
-
-app.listen(desiredport, () => {
-    console.log("Server is running on port " + desiredport);
-});
