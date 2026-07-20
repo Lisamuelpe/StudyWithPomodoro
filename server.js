0812928dba62ff00cf762b0536f2f7165d809930
Lisandro <lisandrosamuelpenalugo@gmail.com>
2026-07-20 10:00:48 -0400

    route to frontend in server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-let express = require("express");
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.status(200).send("Hello World!");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 module.exports = app;
