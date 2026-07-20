c77f480527355a38365fb4166bf4e54441be4c38
Lisandro <lisandrosamuelpenalugo@gmail.com>
2026-07-20 12:33:34 -0400

    aaaaa

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const server = express();
 
-server.use(express.static("public"));
+server.use(express.static("./public"));
 
 server.get("/", (req, res) => {
   res.send("Hello World!");
