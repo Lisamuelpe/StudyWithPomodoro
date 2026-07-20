eae2c9998b23c48622ae8d1576826d294ec371b4
Lisandro <lisandrosamuelpenalugo@gmail.com>
2026-07-20 10:02:34 -0400

    trycathimpl

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    try {
+        res.sendFile(path.join(__dirname, "public", "index.html"));
+    } catch (error) {
+        console.error("Error occurred while serving the index page:", error);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 module.exports = app;
