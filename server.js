9a6861e5d862325f04b50d32e57d48120da6479b
Lisandro <lisandrosamuelpenalugo@gmail.com>
2026-07-21 11:50:10 -0400

    vercel no silve

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const path = require("path");
 
 const server = express();
 
-server.use(express.static(path.join(__dirname, "public")))  ;
+server.use(express.static(path.join(__dirname, "public")));
+
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
 
 module.exports = server;
