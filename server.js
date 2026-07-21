8c1676ba8ef4f169297b97a38385138416ede3e3
Lisandro <lisandrosamuelpenalugo@gmail.com>
2026-07-21 11:53:56 -0400

    ay dioooooooooooooo

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const server = express();
 server.use(express.static(path.join(__dirname, "public")));
 
 server.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 module.exports = server;
